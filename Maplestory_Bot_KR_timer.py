5b089df8ee14848f7be9d243f788e22c94f3ed90
Y.W KIM <40012548+kyw624@users.noreply.github.com>
2020-01-01 19:03:36 +0900

    Update Maplestory_Bot_KR_timer.py

--- Maplestory_Bot_KR_timer.py
+++ Maplestory_Bot_KR_timer.py
@@ -13,8 +13,9 @@ async def on_ready():
     while True:
         now = datetime.utcnow().minute
         if now == 29 or now == 59:  # 알림을 받을 시간 지정
-            client.get_channel(channels[0]).send("경뿌 1분 전")  # 보낼 메시지 설정
-            client.get_channel(channels[1]).send("경뿌 1분 전")
+            await client.get_channel(channels[0]).send("경뿌 1분 전")  # 보낼 메시지 설정
+            await client.get_channel(channels[1]).send("경뿌 1분 전")
+            
             time.sleep(60)  # 60초 후 재탐색
         else:
             time.sleep(1)  # 1초 후 재탐색
